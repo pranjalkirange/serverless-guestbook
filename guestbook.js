f21d72ae9ff1176d36a04ec423e791fb614db8d3
pranjalkirange <83036346+pranjalkirange@users.noreply.github.com>
2022-07-10 10:56:43 +0000

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://github.com/pranjalkirange/serverless-guestbook.git';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
